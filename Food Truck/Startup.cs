97c0419eb6b069622054db0b28f80808f83a1f74
Mike Wilson <mdubs08@gmail.com>
2016-11-07 10:06:08 -0600

    Initial Drop
    Initial drop of Food Truck

---
+++ Food Truck/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Food_Truck.Startup))]
+namespace Food_Truck
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
